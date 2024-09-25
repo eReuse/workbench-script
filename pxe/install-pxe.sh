f0b5b91ccd08310d4ebd454f95c93fe5bda1a913
pedro <git2021@cas.cat>
2024-09-25 13:07:43 -0300

    pxe: force cp

--- pxe/install-pxe.sh
+++ pxe/install-pxe.sh
@@ -54,9 +54,9 @@ END
 }
 
 extract_live_parts_for_tftp() {
-        cp -v "${PXE_DIR}/../iso/staging/live/filesystem.squashfs" "${nfs_path}/live/"
-        cp -v "${PXE_DIR}/../iso/staging/live/vmlinuz" "${tftp_path}/"
-        cp -v "${PXE_DIR}/../iso/staging/live/initrd" "${tftp_path}/"
+        cp -fv "${PXE_DIR}/../iso/staging/live/filesystem.squashfs" "${nfs_path}/live/"
+        cp -fv "${PXE_DIR}/../iso/staging/live/vmlinuz" "${tftp_path}/"
+        cp -fv "${PXE_DIR}/../iso/staging/live/initrd" "${tftp_path}/"
 }
 
 install_netboot() {
