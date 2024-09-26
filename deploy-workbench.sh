2a7af1ddabaab592668db4d8f23763da9652358a
pedro <git2021@cas.cat>
2024-09-26 13:24:09 -0300

    Revert "pxe: we know the server host, is in the config"
    This reverts commit 5399d544ee4bec1e427686155d629c118d2a70f1.

--- deploy-workbench.sh
+++ deploy-workbench.sh
@@ -279,7 +279,8 @@ if [ -d /run/live/medium ]; then
         mount --bind /run/live/medium /mnt
         # debian live nfs path is readonly, do a trick
         #   to make snapshots subdir readwrite
-        mount ${server_ip}:/snapshots /run/live/medium/snapshots
+        nfs_host="\$(df -hT | grep nfs | cut -f1 -d: | head -n1)"
+        mount \${nfs_host}:/snapshots /run/live/medium/snapshots
 fi
 # clearly specify the right working directory, used in the python script as os.getcwd()
 cd /mnt
