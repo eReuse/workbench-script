c73997c5c837db2356b1b6470e3027a8c03b0065
pedro <git2021@cas.cat>
2026-05-15 19:43:07 +0200

    refactor wb: make comments easier to read

--- deploy-workbench.sh
+++ deploy-workbench.sh
@@ -368,8 +368,10 @@ else
 fi
 
 set -x
-stty -echo # Do not show what we type in terminal so it does not meddle with our nice output
-dmesg -n 1 # Do not report *useless* system messages to the terminal
+# Do not show what we type in terminal so it does not meddle with our nice output
+stty -echo
+# Do not report *useless* system messages to the terminal
+dmesg -n 1
 
 wb
 
