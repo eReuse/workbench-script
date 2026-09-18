1cb36ec5f5a1aac1a2f633211d30b9f7ee8a058b
pedro <git2021@cas.cat>
2026-09-19 01:32:14 +0200

    bugfix git dubious ownership
    fatal: detected dubious ownership in repository at '/opt/workbench-script'

--- deploy-workbench.sh
+++ deploy-workbench.sh
@@ -590,7 +590,7 @@ main() {
         iso_name="workbench_${VERSION_ISO}"
         hostname='workbench'
         root_passwd='workbench'
-        workbench_version="$(git log --format="%H %ad" --date=iso -n 1)"
+        workbench_version="$(git --git-dir=./.git log --format="%H %ad" --date=iso -n 1)"
 
         eval "${detect_user_str}" && detect_user
 
